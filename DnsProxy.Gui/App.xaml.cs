dcd6a12b5a3a85e85f8df029df5c726576b21855
BLun78 <viper78@gmx.net>
2020-11-08 08:26:35 +0100

    add trayicon

--- DnsProxy.Gui/App.xaml.cs
+++ DnsProxy.Gui/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Hardcodet.Wpf.TaskbarNotification;
 
 namespace DnsProxy.Gui
 {
@@ -13,5 +14,20 @@ namespace DnsProxy.Gui
     /// </summary>
     public partial class App : Application
     {
+        private TaskbarIcon notifyIcon;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            //create the notifyicon (it's a resource declared in NotifyIconResources.xaml
+            notifyIcon = (TaskbarIcon)FindResource("NotifyIcon");
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            notifyIcon.Dispose(); //the icon would clean up automatically, but this is cleaner
+            base.OnExit(e);
+        }
     }
 }
